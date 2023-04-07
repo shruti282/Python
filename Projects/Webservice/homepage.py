24ce455f76c3ad94787553e364a21ae5a0cb622a
Shruti Kadam <shruti9728.sk@gmail.com>
2023-04-07 16:43:21 +0100

    Done

--- Projects/Webservice/homepage.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-homepage = Blueprint(__name__, "homepage")
-
-@homepage.route("/")
-def home():
-    return render_template("index.html", user = "USER")
